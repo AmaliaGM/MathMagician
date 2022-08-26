ce0901efaa306bf96d08f087aa5d0dd1821990e5
Amalia Gomez Moro <103516637+AmaliaGM@users.noreply.github.com>
2022-08-26 18:07:51 -0300

    Revert "Math Magicians: events"

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import Screen from './components/Screen';
-import Wrapper from './components/wrapper';
-import ButtonBox from './components/ButtonBox';
-import Button from './components/Button';
-import CalcProvider from './logic/CalcContext';
+import Calc from './components/calculator';
 
+/*
 const btnValues = [
   ['AC', '+/-', '%', '/'],
   ['7', '8', '9', 'x'],
   ['4', '5', '6', '-'],
   ['1', '2', '3', '+'],
   ['0', '.', '='],
-];
+]; */
 
 function App() {
   return (
-    <CalcProvider>
-      <Wrapper>
-        <Screen />
-        <ButtonBox>
-          {btnValues.flat().map((btn, i) => (
-            <Button
-              value={btn}
-              key={i}
-            />
-          ))}
-        </ButtonBox>
-      </Wrapper>
-    </CalcProvider>
+    <Calc />
   );
 }
 

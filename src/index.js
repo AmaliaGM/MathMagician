6107b1bed66d2068c903657376f7a0e7cda8c96c
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-09-01 20:33:39 -0300

    netlify fix, HashRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,9 +7,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 /*   ReactDOM.render(<App/>,document.getElementById('root'))
  */);
